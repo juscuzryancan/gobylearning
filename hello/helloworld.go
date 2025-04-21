1f31032fda7cb131953510e1fa2fa57b6e644eda
Ryan Riley Puzon <rrgpuzon@gmail.com>
2025-04-21 12:53:59 -0500

    Finishes hello in learngo

--- hello/helloworld.go
+++ hello/helloworld.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
-const englishPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	englishPrefix = "Hello, "
+	spanishPrefix = "Hola, "
+	frenchPrefix  = "Bounjour, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Ryan"))
+	fmt.Println(Hello("Ryan", ""))
 }
